e6aef6fe9e491cbbb958984a0e0287b23a9d98e2
Carlos Castro Brenes <ccasbre27@hotmail.com>
2020-04-04 15:25:50 -0600

    add onSubmit event handler

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term + ' app');
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
